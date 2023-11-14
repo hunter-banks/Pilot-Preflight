090e7dcb071a2e5f3617942d9407d91fe62c6761
jayle_mochi <jaylene.baltazar@warriorlife.net>
2023-11-14 14:06:04 -0800

    main and activity main set up for other pages to be added

--- app/src/main/java/com/appdev/pilot_preflight/MainActivity.kt
+++ app/src/main/java/com/appdev/pilot_preflight/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.appdev.pilot_preflight
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val regulationsButton: Button = findViewById(R.id.regulationsButton)
+        regulationsButton.setOnClickListener {
+            startActivity(Intent(this, RegulationsActivity::class.java))
+        }
+
+        val medicalButton: Button = findViewById(R.id.medicalButton)
+        medicalButton.setOnClickListener {
+            startActivity(Intent(this, MedicalActivity::class.java))
+        }
     }
 }
