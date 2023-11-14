74c4bb3a53a606aadc4fd955638ea8f4030d1b7f
jayle_mochi <jaylene.baltazar@warriorlife.net>
2023-11-14 14:07:03 -0800

    regulations page and layout basic implementation

---
+++ app/src/main/java/com/appdev/pilot_preflight/RegulationsActivity.kt
@@ -0,0 +1,13 @@
+package com.appdev.pilot_preflight
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegulationsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regulations)
+        // Configure the layout elements for the regulations screen
+        // Set text for title and information box
+    }
+}
