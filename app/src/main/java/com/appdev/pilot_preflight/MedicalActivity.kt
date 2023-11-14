941f8b5b1334106d0e260808ecca8d8b31a076ab
jayle_mochi <jaylene.baltazar@warriorlife.net>
2023-11-14 14:06:46 -0800

    medical page and layout basic implementation

---
+++ app/src/main/java/com/appdev/pilot_preflight/MedicalActivity.kt
@@ -0,0 +1,13 @@
+package com.appdev.pilot_preflight
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MedicalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_medical)
+        // Configure the layout elements for the medical screen
+        // Set up buttons that link to different websites
+    }
+}
